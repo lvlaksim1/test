20dcdacb6cd1fbeade40b992590660d7a766bfca
lvlaksim1 <lvlaksim@gmail.com>
2026-08-28 03:24:45 +0300

    Prepare release 24

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "23",
+  version: "24",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -47,7 +47,7 @@ const config: ExpoConfig = {
     predictiveBackGestureEnabled: false,
     softwareKeyboardLayoutMode: "resize",
     package: env.androidPackage,
-    versionCode: 123,
+    versionCode: 124,
     permissions: ["POST_NOTIFICATIONS"],
   },
   web: {
