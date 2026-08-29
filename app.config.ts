950aaac887746e2e98bf0167e19e348ef4747ff9
lvlaksim1 <lvlaksim@gmail.com>
2026-08-29 06:42:50 +0300

    Set version 25

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.21",
+  version: "1.0.25",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
@@ -47,7 +47,7 @@ const config: ExpoConfig = {
     predictiveBackGestureEnabled: false,
     softwareKeyboardLayoutMode: "resize",
     package: env.androidPackage,
-    versionCode: 121,
+    versionCode: 125,
     permissions: ["POST_NOTIFICATIONS"],
   },
   web: {
